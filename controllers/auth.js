854a84361d4bfc0a8cfd05fa3d05bdb23d457ebd
Babenko Liudmyla <savelieva0509@gmail.com>
2023-11-14 15:40:36 +0200

    add token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const { User } = require("../models/user");
 const { controllerWrapper, HttpError } = require("../helpers");
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -32,7 +35,11 @@ const login = async (req, res) => {
     throw HttpError(401, "Email or password is wrong");
   }
 
-  const token = "kjhbvcxcvgbhnjm";
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
   res.json({
     token,
   });
