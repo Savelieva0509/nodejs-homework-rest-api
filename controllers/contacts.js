0fb57c4b6692ec64cba2cd6bf28ba970d2cad89d
Babenko Liudmyla <savelieva0509@gmail.com>
2023-11-17 16:01:12 +0200

    add pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,7 +3,12 @@ const { controllerWrapper, HttpError } = require("../helpers");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner });
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, { skip, limit }).populate(
+    "owner",
+    "name email"
+  );
   res.json(result);
 };
 
