4107d51c4b5f054b63033a7a06b9e01e1ee5b9c5
Liudmyla Babenko <104641072+Savelieva0509@users.noreply.github.com>
2023-11-27 15:33:59 +0200

    Hw05 avatars (#4)
    * add multer
    
    * add start files
    
    * add folders for avatar
    
    * add gravatar
    
    * add upload
    
    * add avatarURL in SchemaUser
    
    * add updateAvatar controller
    
    * add router avatars
    
    * change img size
    
    * start test
    
    * fix mentor

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
 const controller = require("../../controllers/auth");
-const { validateBody, authenticate } = require("../../middlewares");
+const {
+  validateBody,
+  authenticate,
+  upload,
+  validateAvatar,
+} = require("../../middlewares");
 const { schemas } = require("../../models/user");
 
 const router = express.Router();
@@ -17,5 +22,13 @@ router.post("/login", validateBody(schemas.loginSchema), controller.login);
 router.get("/current", authenticate, controller.getCurrent);
 
 router.post("/logout", authenticate, controller.logout);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  validateAvatar,
+  controller.updateAvatar
+);
 
 module.exports = router;
